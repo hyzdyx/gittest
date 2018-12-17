bc7244d848be903f6bb9ff5d9b144431611cec97
h <hyzdyx@foxmail.com>
2018-12-17 13:16:46 +0800

    提交1

---
+++ .idea/java/Test1.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: huangyz
+ * @Date: 2018-12-17 13:13
+ * @Version 1.0
+ */
+public class Test1 {
+    public static void main(String[] args){
+        System.out.println(1);
+    }
+}
